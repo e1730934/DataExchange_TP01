5a6095f26012c6b31962664f20a7e5d3d17b8fb7
Encarnacion Lorry James <enloja@gmail.com>
2022-03-14 01:46:59 +0000

    Upload New File

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const router = require('./router');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.json()); // http://expressjs.com/en/resources/middleware/body-parser.html
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`L'API peut maintenant recevoir des requêtes: http://localhost:${port}`);
+});
